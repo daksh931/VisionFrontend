2ec08f915bfec8a3ada354aa2170d4c13671926d
dalip03 <dalipdivakar@gmail.com>
2025-02-16 21:55:31 +0530

    Courses Scroll

--- src/assets/Components/HomePage.jsx
+++ src/assets/Components/HomePage.jsx
@@ -3,12 +3,19 @@ import SimpleSlider from "./SimpleSlider";
 import TestimonialSection from "./TestimonialSection";
 import WeOffer from "./WeOffer";
 import InquirySection from "./InquirySection";
+<<<<<<< Updated upstream
+=======
+import MarqueeDemo from "./MagicUI/marqueeDemo";
+import ScrollingTestimonials from "./ScrollTransition";
+
+>>>>>>> Stashed changes
 export default function HomePage() {
   return (
     <div>
 
       <SimpleSlider/>
-      <MarqueeCourses/>
+      {/* <MarqueeCourses/> */}
+      <ScrollingTestimonials/>
       <WeOffer/>
       <InquirySection/>
       <TestimonialSection/>
